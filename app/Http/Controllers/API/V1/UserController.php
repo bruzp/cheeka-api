ab5d7711176be66e6c054bb33b4e9d927fdc554c
Bruce <bruce@ipp.ph>
2022-10-19 15:18:06 +0800

    update profile

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -2,15 +2,33 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function show(Request $request)
     {
         return response()->json(new UserResource($request->user()));
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['nullable', 'string'],
+            'email' => ['nullable', 'email', Rule::unique('users')->ignore($request->user())],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['messages' => $validator->errors()]);
+        }
+
+        $request->user()->update($validator->validated());
+
+        return response()->json(['messages' => __('user.updated')]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function(){
     Route::middleware('auth:sanctum')->group(function(){
         Route::get('/user', [UserController::class, 'show']);
+        Route::put('/user', [UserController::class, 'update']);
 
         Route::post('/logout', [LoginController::class, 'logout']);
     });
